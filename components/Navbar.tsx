23be0b66f15f33724397141edac182f67092a74b
Tung Teng <thetungctn@gmail.com>
2026-04-01 20:55:02 +0700

    Add Collections nav item

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,20 +2,23 @@ import Link from 'next/link'
 
 const navItems = [
   { href: '/', label: 'Home' },
+  { href: '/collections', label: 'Collections' },
   { href: '/about', label: 'About' },
   { href: '/contact', label: 'Contact' },
 ]
 
 const Navbar = () => {
   return (
-    <nav className="fixed w-full flex justify-between items-center z-2 p-4">
-      <div />
-      <div className="flex items-center gap-[clamp(1rem,4vw,2rem)]">
+    <nav className="fixed w-full flex justify-between items-start md:items-center z-2 p-8 md:p-4">
+      <div className="uppercase font-condensed font-extrabold text-2xl md:px-4">
+        Loose Frame
+      </div>
+      <div className="flex flex-col md:flex-row items-end md:items-center gap-0.5 md:gap-[max(2vw,2rem)]">
         {navItems.map(({ href, label }) => (
           <Link
             key={href}
             href={href}
-            className="text-lg font-medium -tracking-[2%] p-4"
+            className="text-base md:text-lg font-medium -tracking-[2%] p-0 md:p-4"
           >
             {label}
           </Link>
